aa51057fa444966c10c6f7226b038af9f6004928
digimon128257 <126612108+digimon128257@users.noreply.github.com>
2023-06-17 07:35:32 -0500

    First Commit

---
+++ EasyFinanceLoan.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EasyFinanceLoan.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EasyFinanceLoan.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyFinanceLoan.Mobile;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
